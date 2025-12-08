e9420739da20deb77c228680864e9c224d1ab581
Paul Bob <paul.ionut.bob@gmail.com>
2025-12-08 11:58:44 +0200

    1 day schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -19,7 +19,6 @@
 
 # Learn more: http://github.com/javan/whenever
 
-# every 1.day, at: "2:00am" do
-every 1.minutes do
+every 1.day, at: "2:00am" do
   command "cd /home/deploy/kanban_avodemo_com/current && DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:seed"
 end
